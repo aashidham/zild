d8de294f1e6b8ce7a8d0c779adfffc961eb17b52
Aaditya Shidham <aashidham@gmail.com>
2017-03-17 07:17:20 -0700

    changed to child_pty and added zild task manager

--- process_manager.js
+++ process_manager.js
@@ -7,9 +7,12 @@
 var fs = require('fs');
 var net = require('net');
 var spawn = require('child_process').spawn;
-
 var path = require('path');
+
 var root_dir = require('os').homedir();
+
+var spawn2 = require('child_pty').spawn;
+var ON_DEATH = require('death');
 
 var id0 = process.argv[2];
 var project_dir = path.join(root_dir,".zild", id0);
@@ -18,6 +21,7 @@ var project_config = JSON.parse(fs.readFileSync(path.join(project_dir,"config.js
 var sock_file = path.join(project_dir, "s.sock");
 
 var server = net.createServer(function(sock) {
+	//sock.write("\n");
 	sock_arr.push(sock);
 }).listen(sock_file);
 
@@ -25,12 +29,13 @@ var sock_arr = [];
 
 var print_out = function(data)
 {
-	//console.log(data);
+	//log to STDOUT will be ignored by z, but if this is run directly, can be viewed for debugging purposes
+	console.log(data);
 	for (var i = 0, len = sock_arr.length; i < len; i++) {
 		var sock0 = sock_arr[i];
 		if(sock0 && !sock0.destroyed)
 		{
-			sock0.write(data);
+			sock0.write(""+data);
 		}		
 	}
 }
@@ -46,8 +51,8 @@ var write3 = function(data)
 
 var add_cb = function(proc_curr)
 {
-	proc_curr.stdout.setEncoding('utf8');
-	proc_curr.stderr.setEncoding('utf8');
+	//proc_curr.stdout.setEncoding('utf8');
+	//proc_curr.stderr.setEncoding('utf8');
 	proc_curr.stdout.on('data', print_out);
 	proc_curr.stderr.on('data', print_out); 
 	//we need these listeners to close this manager when the process it manages closes
@@ -70,9 +75,8 @@ var add_cb = function(proc_curr)
 }
 
 //add_cb(spawn("bash",["-c","ping 8.8.8.8"]));
-add_cb(spawn("bash",["-c",project_config.bash_cmd], {cwd: project_config.cwd } ));
+add_cb(spawn2( process.env.SHELL || "bash",["-c",project_config.bash_cmd], {cwd: project_config.cwd } ));
 
-var ON_DEATH = require('death');
 ON_DEATH(function(signal, err) {
 	server.close();
 	process.exit();
