e4e6c2653201bd61a69fc93517164cfcb78cd6d6
Aaditya Shidham <aashidham@gmail.com>
2017-03-26 03:14:33 -0700

    changed stderr to string, added graceful close

--- process_manager.js
+++ process_manager.js
@@ -35,56 +35,64 @@ var global_config = JSON.parse(fs.readFileSync(global_config_path));
 project_config.token = global_config.token;
 project_config.hostname = global_config.hostname;
 project_config.cmp_name = global_config.cmp_name;
+var child_dead = false;
 
 
 var sock_file = path.join(project_dir, "s.sock");
 var quit_ref_count = 0;
 var data_lines_count = 0;
 
+//listeners
+events_intersect("post_response_done", "self_done", post_events, function() {
+	//console.log("PRD "+quit_ref_count); 
+	if(quit_ref_count < 1) process.exit();
+})
 
+//death listener has to come before server so that it can close the server if process is dying
+//TODO: is this right?
+ON_DEATH(function(signal, err) {
+	graceful_close("from procmon death");
+});
 
+var sock_arr = [];
 var server = net.createServer(function(sock) {
 	//sock.write("\n");
+	sock.on('data', function(d){
+		if((JSON.parse(d)).ping) sock.write(JSON.stringify({'pong':uuid()}));
+	});
 	sock_arr.push(sock);
 }).listen(sock_file);
 
-//TODO: figure out why this won't detect rm s.sock
-//inside eager_fermat_1490043212442
-//after figuring this out, we have to disable this because server.close() invokes this and 
-//doesnt wait for POST calls
-server.on("close", function(){
-	//console.log("detected close");
-	//process.exit();
-})
-
-server.on("end", function(){
-	//console.log("detected end");
-	//process.exit();
-})
 
 fs.watch(project_dir, (eventType, filename) => { 
 	if(filename === "s.sock"){
-		console.log([eventType, filename]);
-		console.log(fs.existsSync(sock_file));
-		console.log("[[socket file deleted?]]");
+		//console.log([eventType, filename]);
+		if(!fs.existsSync(sock_file)) {
+			graceful_close("from socket file removal");
+		}
+		//server.close(function(e){console.log(e);}); 
+		// above releases an error because server already closed when this is invoked from server.close(), but this is still needed when file 
+		// is manually removed
+		
 	}
 });
 
 
-var sock_arr = [];
+
 
 
 var print_out = function(data)
 {
-	console.log("B");
 	var data = ""+data;
 	//log to STDOUT will be ignored by z, but if this is run directly, can be viewed for debugging purposes
 	console.log(data);
 	for (var i = 0, len = sock_arr.length; i < len; i++) {
 		var sock0 = sock_arr[i];
-		if(sock0 && !sock0.destroyed)
+		//TODO: figure out if I need sock0.writable if I can catch the error in the if block, uncomment epipe.d for this
+		if(sock0 && sock0.writable)
 		{
 			sock0.write(data);
+			sock0.on('error', function(e) {console.log(e); /*fs.writeFileSync("epipe.d", e);*/})
 		}		
 	}
 }
@@ -97,16 +105,15 @@ var write3_common = function(data, stderr)
 	var send_obj = JSON.parse(JSON.stringify(project_config));
 	send_obj.data = data;
 	send_obj.data_chunk = data_lines_count;
-	send_obj.stderr = ""+stderr;
+	send_obj.stderr = stderr;
 	console.log(send_obj);
 	quit_ref_count++;
-	console.log("A");
 	request.post( 'https://zild.io/shell_data', {json: send_obj}, 
 		function(e,r,b){
 			if(e) {console.log(e); }
 			quit_ref_count--;
-			console.log("shell_data result");
-			console.log(b);
+			//console.log("shell_data result");
+			//console.log(b);
 			post_events.emit("post_response_done");
 		});
 	print_out(data);
@@ -115,9 +122,9 @@ var write3_common = function(data, stderr)
 
 
 //make POST call, write to log, write to IPC
-var write3_stdout = function(data){write3_common(data, false);}
+var write3_stdout = function(data){write3_common(data, "false");}
 
-var write3_stderr = function(data){write3_common(data, true);}
+var write3_stderr = function(data){write3_common(data, "true");}
 
 
 var add_cb = function(proc_curr)
@@ -146,13 +153,15 @@ var add_cb = function(proc_curr)
 			post_events.emit("post_response_done");
 		});
 
-	proc_curr.stdout.on('data', write3_stdout);
 	proc_curr.stderr.on('data', write3_stderr); 
+	proc_curr.stdout.on('data', write3_stdout);
 	//we need these listeners to close this manager when the process it manages closes
 	//otherwise the server prevents natural closing
-	proc_curr.on('error', function(e) { print_out(e); server.close(); process.exit(); });
+	proc_curr.on('error', function(e) { print_out(e); graceful_close("from process error"); });
 	proc_curr.on('close', function(code, signal) { 
-		var close_code = ""+code;
+		var close_code = " ";
+		child_dead = true;
+		if(code) close_code = close_code + " " +code;
 		if(signal) close_code = close_code + " " +signal;
 		quit_ref_count++;
 		request.post( 'https://zild.io/stop_process', {json: {token: project_config.token, close_code: close_code, process_id: project_config.process_id}}, 
@@ -168,16 +177,37 @@ var add_cb = function(proc_curr)
 			else print_out("Process closed with code "+code+" and signal "+signal+"\n");	
 		} 
 		server.close(); 
-		//TODO: this may not be the correct solution, since it is possible to emit before anybody is listening
-		//then what if this event never gets emitted later? practically this hasnt happened but it is weird
-		post_events.on("post_response_done", function() {if(quit_ref_count < 1) process.exit()});
+		post_events.emit("self_done");
 	})
+}
+
+function graceful_close(msg)
+{
+	//process has never started and since post calls are made after pid is set, we also know no post calls are made
+	//TODO: try to trigger this, is emitting self_done right? or must kill self immediately?
+	if(!project_config.pid) { console.log(msg+": found process was never started killing self immediately"); post_events.emit("self_done"); }
+	//process has been started but not killed
+	if(project_config.pid && !child_dead) { console.log(msg+": found process is not dead yet, so killing it"); process.kill(project_config.pid, 'SIGKILL'); }
+	//process has been killed
+	else console.log(msg+": found process is dead, so doing nothing");
+}
+
+function events_intersect(ev1, ev2, ee, f0)
+{
+	ee.on(ev1, function() { 
+		ee.on(ev2, function() { 
+			//console.log(ev1 +" then "+ev2); 
+			f0(); 
+		}); 
+	});
+
+	ee.on(ev2, function() { 
+		ee.on(ev1, function() { 
+			//console.log(ev2 +" then "+ev1); 
+			f0(); 
+		}); 
+	});
 }
 
 //add_cb(spawn("bash",["-c","ping 8.8.8.8"]));
 add_cb(spawn2( process.env.SHELL || "bash",["-c",project_config.bash_cmd], {cwd: project_config.cwd } ));
-
-ON_DEATH(function(signal, err) {
-	server.close();
-	process.exit();
-});
